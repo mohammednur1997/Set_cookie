c49ee5c52621b907d0e1ee51718b9b66cfcf80bb
Mohammed Nur <48428664+mohammednur1997@users.noreply.github.com>
2020-03-22 16:36:26 +0300

    initial commit

---
+++ set_cookie.php
@@ -0,0 +1,38 @@
+	
+	
+	<?php
+	if(isset($_COOKIE['vesite']))
+	{
+		//you are re-visiteing our site,,thank you very much
+	    $name="vesite";
+		$count=$_COOKIE['vesite']+1;
+		$expair=time()+(60*60*24*7*1);
+	    setcookie($name,$count,$expair);
+
+		
+	}else
+	{
+		$name="vesite";
+     	$count=1;
+	    $expair=time()+(60*60*24*7*1);
+	    setcookie($name,$count,$expair);
+	}
+	?>
+
+	<!DOCTYPE HTML>
+		<html lang="en-US">
+		<head>
+			<meta charset="UTF-8">
+			<title></title>
+		</head>
+		<body>
+		<h1>Set Cookie For your website.......</h1>
+		
+		   <h3>
+		  <?php
+		  echo "You visite our site=".$_COOKIE['vesite']."-time(s)";
+		  ?>
+		 </h3>
+		 
+		</body>
+		</html>
